78f41497e391c836394fd3a9080c389b299ea488
RIDONE HOSSAIN MOON <168392332+RidOneMoon@users.noreply.github.com>
2025-12-17 23:00:55 +0800

    solve on cors in app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use(/.*/, cor({
+app.use(/.*/, cors({
     origin: allowedOrigin,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
